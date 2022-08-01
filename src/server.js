6dd485a2fd221aa9fe3d725d3ef2d9eb9b925017
bigperson <daein7076@naver.com>
2022-08-01 23:57:40 +0900

    (3.1 ~ 3.2) get Request

--- src/server.js
+++ src/server.js
@@ -4,7 +4,14 @@ const PORT = 4000;
 
 const app = express();
 
+// application 설정
+// app = express()로 application 만듦
+// listen(PORT, handleListening) 으로 외부 접속 시작(listen)
+// 그 사이에 application값 설정!!
+
+app.get("/", () => console.log("Somebody is trying to go home."));
+
 const handleListening = () =>
-  console.log(`Sever listening on port http://localhost:${PORT}`);
+  console.log(`✅✅ Sever listening on port http://localhost:${PORT} ✅✅`);
 
 app.listen(PORT, handleListening);
