3d1e7d70f595c91cdf099bc610ceb1927d6b8db6
bigperson <daein7076@naver.com>
2022-08-02 21:41:20 +0900

    (3.11) external middlewares

--- src/server.js
+++ src/server.js
@@ -1,35 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
 // application 설정
 // app = express()로 application 만듦
 // listen(PORT, handleListening) 으로 외부 접속 시작(listen)
 // 그 사이에 application값 설정!!
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  } else {
-    console.log("Allowed, you may continue");
-    next();
-  }
-};
-
 const handleHome = (req, res) => {
   return res.send("<h1>I'm still wating you</h1>");
 };
 
 app.use(logger);
-app.use(privateMiddleware);
 app.get("/", handleHome);
 
 const handleListening = () =>
