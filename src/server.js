5eba50a8fc93087162e10d1ef20c81be94e6c333
bigperson <daein7076@naver.com>
2022-08-09 16:50:32 +0900

    (4.1) making our routers
    1. app.use('router address', router name)
    2. const router = express.Router();
    3. router.get('address', function name)
    4. function ~

--- src/server.js
+++ src/server.js
@@ -4,19 +4,41 @@ import morgan from "morgan";
 const PORT = 4000;
 
 const app = express();
+//log setting and execute
 const logger = morgan("dev");
+app.use(logger);
+
+//router setting
+//global router
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+//user router
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+//video router
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
 
 // application 설정
 // app = express()로 application 만듦
 // listen(PORT, handleListening) 으로 외부 접속 시작(listen)
 // 그 사이에 application값 설정!!
 
-const handleHome = (req, res) => {
-  return res.send("<h1>I'm still wating you</h1>");
-};
-
-app.use(logger);
-app.get("/", handleHome);
+// router
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () =>
   console.log(`✅✅ Sever listening on port http://localhost:${PORT} ✅✅`);
