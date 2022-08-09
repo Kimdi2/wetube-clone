a4175a6897288e6762576f604554a6d04f46e1a1
bigperson <daein7076@naver.com>
2022-08-10 01:12:26 +0900

    Update videoRouter.js
    regular expressions
    in (\\+d)
    (\+d) means select only numbers
    and first \ is use for JS

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,8 +5,8 @@ const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
 //:id -> parameter
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id/edit(\\d+)", edit);
+videoRouter.get("/:id/delete(\\d+)", deleteVideo);
 
 export default videoRouter;
