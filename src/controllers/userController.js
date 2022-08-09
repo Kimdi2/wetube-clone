3325ca7c1a1986a69057ac4b6b86d7bcd45d1300
bigperson <daein7076@naver.com>
2022-08-10 01:02:41 +0900

    (4.7) url parameters part.1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,4 +4,7 @@ export const login = (req, res) => res.send("Login");
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
 export const logout = (req, res) => res.send("Log Out");
-export const see = (req, res) => res.send("See User");
+export const see = (req, res) => {
+  console.log(req.params);
+  return res.send("See User");
+};
