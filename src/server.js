f96fe34ce97342f901a24baafbee8db59a7cdb54
bigperson <daein7076@naver.com>
2022-08-09 17:11:43 +0900

    (4.2) cleaning the code(about router)

--- src/server.js
+++ src/server.js
@@ -1,34 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
 const PORT = 4000;
 
 const app = express();
 //log setting and execute
 const logger = morgan("dev");
 app.use(logger);
-
-//router setting
-//global router
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-//user router
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-//video router
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
 
 // application 설정
 // app = express()로 application 만듦
