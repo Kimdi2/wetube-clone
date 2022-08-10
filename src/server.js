d1c7cbf520a3deb1268b324b795b1cfa66867f38
bigperson <daein7076@naver.com>
2022-08-10 15:56:10 +0900

    (5.1) configuring Pug

--- src/server.js
+++ src/server.js
@@ -16,35 +16,8 @@ app.use(logger);
 // app = express()로 application 만듦
 // listen(PORT, handleListening) 으로 외부 접속 시작(listen)
 // 그 사이에 application값 설정!!
-const URLLogger = (req, res, next) => {
-  console.log(`Path: ${req.path}`);
-  next();
-};
-const timeLogger = (req, res, next) => {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = now.getMonth() + 1;
-  const date = now.getDate();
-  console.log(`Time: ${year}.${month}.${date}`);
-  next();
-};
-const securityLogger = (req, res, next) => {
-  if (req.protocol === "http") {
-    console.log("Insecure");
-  } else {
-    console.log("Secure");
-  }
-  next();
-};
-const protectorMiddleware = (req, res, next) => {
-  if (req.path === "/protected") {
-    return res.send("Forbidden");
-  }
-  next();
-};
 
-app.use(URLLogger, timeLogger, securityLogger, protectorMiddleware);
-
+app.set("view engine", "pug");
 // router
 app.use("/", globalRouter);
 app.use("/users", userRouter);
