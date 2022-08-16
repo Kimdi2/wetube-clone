2322236ca223a9c7e6e7a03ab6223ecff573c006
bigperson <daein7076@naver.com>
2022-08-16 20:34:37 +0900

    (6.0) array database part 1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,33 +1,37 @@
+let videos = [
+  {
+    title: "First video",
+    rating: 4,
+    comments: 2,
+    createAt: "32 minutes ago",
+    views: 512,
+    id: 1,
+  },
+  {
+    title: "Second video",
+    rating: 2,
+    comments: 0,
+    createAt: "2 minutes ago",
+    views: 52,
+    id: 2,
+  },
+  {
+    title: "Third video",
+    rating: 5,
+    comments: 72,
+    createAt: "3 hours ago",
+    views: 12510,
+    id: 3,
+  },
+];
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First video",
-      rating: 4,
-      comments: 2,
-      createAt: "32 minutes ago",
-      views: 512,
-      id: 1,
-    },
-    {
-      title: "Second video",
-      rating: 2,
-      comments: 0,
-      createAt: "2 minutes ago",
-      views: 52,
-      id: 2,
-    },
-    {
-      title: "Third video",
-      rating: 5,
-      comments: 72,
-      createAt: "3 hours ago",
-      views: 12510,
-      id: 3,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const see = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 
 export const search = (req, res) => res.send("Search");
