659d9dc2b10794ffc4d39a83b93f915bdcdc2776
bigperson <daein7076@naver.com>
2022-08-19 22:39:17 +0900

    (6.24) statics !!!

--- src/models/Video.js
+++ src/models/Video.js
@@ -22,9 +22,8 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
-// Middlewares
-videoSchema.pre("save", async function () {
-  this.hashtags = this.hashtags[0]
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
     .split(",")
     .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
