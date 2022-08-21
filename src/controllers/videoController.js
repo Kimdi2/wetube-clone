0eacf535c3750fc54ceea4ea9ea700be97f79f91
bigperson <daein7076@naver.com>
2022-08-21 21:03:38 +0900

    (6.26 ~ 6.27) search & regex & request

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,7 +5,7 @@ import Video from "../models/Video";
 // });
 export const home = async (req, res) => {
   // promise style
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -68,4 +68,15 @@ export const deleteVideo = async (req, res) => {
   return res.redirect("/");
 };
 
-export const search = (req, res) => res.send("Search");
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}`, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
+};
