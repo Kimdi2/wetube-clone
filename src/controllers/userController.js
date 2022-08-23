e5b650545c4328d8b7b01f94281ac9a2d6eb39b8
bigperson <daein7076@naver.com>
2022-08-23 20:12:18 +0900

    (7.3) form validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,7 +4,38 @@ export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Join" });
 };
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  // username과 email을 검사하는 첫번째 방법
+  // const usernameExists = await User.exists({ username });
+  // if (usernameExists) {
+  //   return res.render("join", {
+  //     pageTitle,
+  //     errorMessage: "This username is already taken.",
+  //   });
+  // }
+  // const emailExists = await User.exists({ email });
+  // if (emailExists) {
+  //   return res.render("join", {
+  //     pageTitle,
+  //     errorMessage: "This Email is already taken.",
+  //   });
+  // }
+
+  //  username과 email을 검사하는 두번째($or) 방법
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
   await User.create({
     name,
     username,
