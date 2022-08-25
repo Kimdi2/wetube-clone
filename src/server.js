bd54ae3080e35104acfaad7123258b271aadd548
bigperson <daein7076@naver.com>
2022-08-25 22:46:47 +0900

    (7.14) Expiration & secrets !!!

--- src/server.js
+++ src/server.js
@@ -25,10 +25,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "Hello",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+    // cookie의 만료시간(miliseconds) 후 사라짐
+    // cookie: {
+    //   maxAge: 20000,
+    // },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
